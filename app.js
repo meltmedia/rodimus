7487adeb93c7a3d346c609014226fa6f06234c2f
Jimmy King <jimmy.king@meltmedia.com>
2013-04-12 08:45:25 -0700

    Express setup

--- app.js
+++ app.js
@@ -1,2 +1,23 @@
-var express = require('express');
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express'),
+    http = require('http');
+
 var app = express();
+
+// all environments
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+})
+
+app.get('/', function(req, res){
+  res.send('Hello, world!');
+});
+
+// development only
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
