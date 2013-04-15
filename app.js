ac4bc048499c76c0bdd2c2c6c60918c888494f02
Jimmy King <jimmy.king@meltmedia.com>
2013-04-15 08:39:52 -0700

    Added uploade handler

--- app.js
+++ app.js
@@ -4,7 +4,8 @@
  */
 
 var express = require('express'),
-    http = require('http');
+    http = require('http'),
+    fs = require('fs');
 
 var app = express();
 
@@ -26,7 +27,37 @@ app.get('/', function(req, res){
   res.render('home', {title: 'Rodimus'});
 });
 
+app.get('/:uniq_dir', function(req, res){
+  res.render('transforming', {title: 'Rodimus is transforming your document.'});
+  res.send('File uploaded to: public/docs/' + req.params.uniq_dir + req.files.document.name + ' - ' + req.files.document.size + ' bytes');
+});
 
+/*
+app.post('/file-upload', function(req, res, next) {
+  console.log(req.body);
+  console.log(req.files);
+});
+*/
+
+// upload handler
+app.post('/file-upload', function(req, res) {
+  // get the temporary location of the file
+  var tmp_path = req.files.document.path;
+  var uniq_dir = tmp_path.split('/')[2];
+  // set where the file should actually exist
+  var target_path = './public/docs/' + uniq_dir + '/' + req.files.document.name;
+  fs.mkdir('./public/docs/' + uniq_dir);
+  // move the file to the intended location
+  fs.rename(tmp_path, target_path, function(err) {
+    if (err) console.log(err);
+    // delete the temporary file
+    fs.unlink(tmp_path, function() {
+      if (err) throw err;
+      //res.send('File uploaded to: ' + target_path + ' - ' + req.files.document.size + ' bytes');
+      res.redirect('/' + uniq_dir);
+    });
+  });
+});
 
 // development only
 http.createServer(app).listen(app.get('port'), function(){
